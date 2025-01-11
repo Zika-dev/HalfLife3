7c04a1888792907c529be30775d3c6e0db58a5ca
Zika-dev <arvid.zika@me.com>
2025-01-11 01:07:43 +0100

    3D compatibility
    Added a second camera that uses a 3D renderer to render to a plane used as an overlay.

---
+++ Assets/Camera3DRescale.cs
@@ -0,0 +1,39 @@
+using Unity.VisualScripting;
+using UnityEngine;
+using static UnityEditor.ShaderGraph.Internal.KeywordDependentCollection;
+
+public class Camera3DRescale : MonoBehaviour
+{
+
+    public Camera camera;
+    public Camera _3Dcamera;
+    public float distance = 10.0f;
+    public GameObject projectionPlane;
+    public RenderTexture renderTexture;
+
+    // Start is called once before the first execution of Update after the MonoBehaviour is created
+    void Start()
+    {
+        _3Dcamera.enabled = false;
+        renderTexture.width = Screen.width;
+        renderTexture.height = Screen.height;
+
+        float height = 2.0f * Mathf.Tan(0.5f * camera.fieldOfView * Mathf.Deg2Rad) * distance;
+        float width = height * Screen.width / Screen.height;
+
+        _3Dcamera.orthographicSize = height / 2;
+
+        Debug.Log("Screen Width: " + Screen.width + " Screen Height: " + Screen.height);
+
+        Debug.Log("Height: " + height + " Width: " + width);
+
+        projectionPlane.transform.localScale = new Vector3(width / 10, 1, height / 10);
+        _3Dcamera.enabled = true;
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+}
